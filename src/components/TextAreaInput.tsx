ea9a67d04c569755643e49054566abec1f220938
unknown <marlon_tancredo@hotmail.com>
2023-09-13 21:47:03 +0100

    feat: implementing TextAreaInput.tsx

---
+++ src/components/TextAreaInput.tsx
@@ -0,0 +1,21 @@
+interface TextAreaInputProps {
+    label?: string;
+    onChangeInput: (value: string) => void;
+}
+
+const TextAreaInput: React.FC<TextAreaInputProps> = ({
+    label,
+    onChangeInput,
+}) => {
+    return (
+        <>
+            <label htmlFor="TextAreaInput">{label}</label>
+            <textarea
+                id="TextAreaInput"
+                onChange={(e) => onChangeInput(e.target.value)}
+            />
+        </>
+    );
+};
+
+export default TextAreaInput;
