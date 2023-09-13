fb684180c009cb88598e61c0947439130e08e60c
unknown <marlon_tancredo@hotmail.com>
2023-09-13 21:46:28 +0100

    feat: implementing SelectInput.tsx

---
+++ src/components/SelectInput.tsx
@@ -0,0 +1,23 @@
+interface SelectInputProps {
+    label?: string;
+    onChangeInput: (value: string) => void;
+}
+
+const SelectInput: React.FC<SelectInputProps> = ({ label, onChangeInput }) => {
+    return (
+        <>
+            <label htmlFor="selectInput">{label}</label>
+            <input
+                list="selectInput"
+                type="select"
+                onChange={(e) => onChangeInput(e.target.value)}
+            />
+            <datalist id="selectInput">
+                <option value="4" />
+                <option value="Not 4" />
+            </datalist>
+        </>
+    );
+};
+
+export default SelectInput;
