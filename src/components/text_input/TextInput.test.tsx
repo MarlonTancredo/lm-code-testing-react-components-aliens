4342c30ee26acca356ddb0fa2c30311109c37520
unknown <marlon_tancredo@hotmail.com>
2023-09-18 18:27:50 +0100

    feat: implementing TextInput test

---
+++ src/components/text_input/TextInput.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import TextInput from "./TextInput"
+
+describe("TextInput component", () => {
+    it("Should render textInput component", () => {
+        const props = {
+            label: "test",
+            value: "test",
+            errorMessage: "test",
+            onChangeInput: () => { }
+        }
+
+        render(<TextInput {...props} />)
+
+        const field = screen.getByLabelText("test")
+
+        expect(field).toBeInTheDocument()
+    })
+})
