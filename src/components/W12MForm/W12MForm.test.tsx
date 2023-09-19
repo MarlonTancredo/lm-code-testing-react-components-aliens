407cd67437fdc10f845fe11289be84a6b95b98c3
unknown <marlon_tancredo@hotmail.com>
2023-09-19 18:26:03 +0100

    refact: isolating elements tests

--- src/components/W12MForm/W12MForm.test.tsx
+++ src/components/W12MForm/W12MForm.test.tsx
@@ -4,23 +4,36 @@ import { labelNames } from './W12MForm';
 
 describe("W12Form component", () => {
 
-	it('should render all form elements', () => {
-		const { specieLabel, planetLabel, beingsQtyLabel, questionLabel, sparingReasonLabel } = labelNames
+	const { specieLabel, planetLabel, beingsQtyLabel, questionLabel, sparingReasonLabel } = labelNames
 
+	it('should render specie input element', () => {
 		render(<W12MForm />);
-
 		const specieInput = screen.queryByLabelText(specieLabel)
-		const planetInput = screen.queryByLabelText(planetLabel)
-		const beingInput = screen.queryByLabelText(beingsQtyLabel)
-		const select = screen.queryByLabelText(questionLabel)
-		const textArea = screen.queryByLabelText(sparingReasonLabel)
-		const submitButton = screen.getByRole("button")
-
 		expect(specieInput).toBeInTheDocument()
+	});
+	it('should render planet input element', () => {
+		render(<W12MForm />);
+		const planetInput = screen.queryByLabelText(planetLabel)
 		expect(planetInput).toBeInTheDocument()
+	});
+	it('should render being input element', () => {
+		render(<W12MForm />);
+		const beingInput = screen.queryByLabelText(beingsQtyLabel)
 		expect(beingInput).toBeInTheDocument()
+	});
+	it('should render question input element', () => {
+		render(<W12MForm />);
+		const select = screen.queryByLabelText(questionLabel)
 		expect(select).toBeInTheDocument()
+	});
+	it('should render sparing input element', () => {
+		render(<W12MForm />);
+		const textArea = screen.queryByLabelText(sparingReasonLabel)
 		expect(textArea).toBeInTheDocument()
+	});
+	it('should render submit button input element', () => {
+		render(<W12MForm />);
+		const submitButton = screen.getByRole("button")
 		expect(submitButton).toBeDisabled()
 	});
 })
