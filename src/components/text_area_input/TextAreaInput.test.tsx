a99844e5d703314ea623116dd5ebfe27a5c0c8c5
unknown <marlon_tancredo@hotmail.com>
2023-09-19 18:57:10 +0100

    feat: implementing new test for TextAreaInput

--- src/components/text_area_input/TextAreaInput.test.tsx
+++ src/components/text_area_input/TextAreaInput.test.tsx
@@ -1,18 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import TextAreaInput from "./TextAreaInput"
 
 const mockOnChangeInput = jest.fn()
 
+const props = {
+    label: "test",
+    onChangeInput: mockOnChangeInput
+}
+
 describe("TextAreaInputComponent", () => {
     it("should render the component TextAreaInputComponent", () => {
-        const props = {
-            label: "test",
-            onChangeInput: mockOnChangeInput
-        }
 
         render(<TextAreaInput {...props} />)
 
         const component = screen.getByLabelText(props.label)
         expect(component).toBeInTheDocument()
+    })
+    it("should able to input a value", () => {
+
+        render(<TextAreaInput {...props} />)
+
+        const component = screen.getByLabelText(props.label)
+        fireEvent.change(component, { target: { value: "test" } })
+        expect(component).toHaveValue("test")
     })
 })
