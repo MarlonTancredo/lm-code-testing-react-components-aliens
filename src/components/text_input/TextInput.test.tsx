26ab5bb590bff3ebf6f6b822e780fe7866fe8295
unknown <marlon_tancredo@hotmail.com>
2023-09-19 17:28:12 +0100

    feat: implementing textinput test

--- src/components/text_input/TextInput.test.tsx
+++ src/components/text_input/TextInput.test.tsx
@@ -4,17 +4,19 @@ import TextInput from "./TextInput"
 describe("TextInput component", () => {
     it("Should render textInput component", () => {
         const props = {
-            label: "test",
-            value: "test",
-            errorMessage: "test",
+            label: "Specie Name:",
+            value: "humans",
+            errorMessage: "Test",
             onChangeInput: () => { }
         }
+        const { label, errorMessage } = props
 
         render(<TextInput {...props} />)
 
-        const field = screen.getByLabelText("test")
+        const field = screen.getByLabelText(label)
+        const message = screen.getByText(errorMessage)
 
         expect(field).toBeInTheDocument()
-
+        expect(message).toBeInTheDocument()
     })
 })
